206582890e107f8861e35588d04ca6bf73766081
euHec <hectorsouzass@gmail.com>
2023-04-11 16:52:56 -0300

    Implementa página de login

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import { Component } from 'react';
+import { Route, Switch } from 'react-router-dom';
 
-function App() {
-  return <div>Hello, TrybeWallet!</div>;
+import Login from './pages/Login';
+import Wallet from './pages/Wallet';
+
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Switch>
+        <Route exact component={ Login } path="/" />
+        <Route exact component={ Wallet } path="/carteira" />
+      </Switch>
+    );
+  }
 }
 
 export default App;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,75 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Login extends React.Component {
+  state = {
+    email: '',
+    password: '',
+    disabled: true,
+  };
+
+  handleChanges = ({ target }) => {
+    const { name, value } = target;
+    this.setState(
+      { [name]: value },
+      () => {
+        const MAX_LENGTH = 6;
+        const { password } = this.state;
+        if (password.length >= MAX_LENGTH) {
+          this.setState({ disabled: false });
+        } else {
+          this.setState({ disabled: true });
+        }
+      },
+    );
+  };
+
+  handleSubmit = () => {
+    const { history } = this.props;
+    history.push('/carteira');
+  };
+
   render() {
-    return <div>Login</div>;
+    const { email, password, disabled } = this.state;
+    return (
+      <div className="containerLogin">
+        <input
+          className="input-login"
+          data-testid="email-input"
+          name="email"
+          onChange={ this.handleChanges }
+          placeholder="exemple@email.com"
+          required
+          type="email"
+          value={ email }
+        />
+        <input
+          className="input-login"
+          data-testid="password-input"
+          name="password"
+          onChange={ this.handleChanges }
+          placeholder="senha@123"
+          required
+          type="password"
+          value={ password }
+        />
+        <button
+          className="button-login"
+          disabled={ disabled }
+          onClick={ this.handleSubmit }
+        >
+          Entrar
+        </button>
+      </div>
+    );
   }
 }
 
 export default Login;
+
+Login.propTypes = {
+  history: PropTypes.shape(
+    PropTypes.func,
+  ).isRequired,
+  push: PropTypes.func.isRequired,
+};
